b4b088aed61feedc728654a8387e893aef910d9c
Adam Reid <adameastcoast@gmail.com>
2017-01-27 00:20:00 -0500

    Boat types and factory

--- src/main/java/com/training/BoatFactory.java
+++ src/main/java/com/training/BoatFactory.java
@@ -4,4 +4,13 @@ package com.training;
  * Created by adame on 1/27/2017.
  */
 public class BoatFactory {
+
+    public Boat makeBoat(BoatTypes value) {
+        switch (value) {
+            case TUGBOAT: return new TugBoat();
+            case SPEEDBOAT: return new SpeedBoat();
+            case PARTYBOAT: return new PartyBoat();
+            default: return null;
+        }
+    }
 }
--- src/main/java/com/training/BoatTypes.java
+++ src/main/java/com/training/BoatTypes.java
@@ -4,4 +4,5 @@ package com.training;
  * Created by adame on 1/27/2017.
  */
 public enum BoatTypes {
+    SPEEDBOAT, PARTYBOAT, TUGBOAT
 }
