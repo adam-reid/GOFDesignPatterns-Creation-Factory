fbc5a79390f3189c5cce146d9652f9e8d73eb1cc
Adam Reid <adameastcoast@gmail.com>
2017-01-27 00:25:22 -0500

    Add main logic

--- src/main/java/com/training/Main.java
+++ src/main/java/com/training/Main.java
@@ -1,10 +1,19 @@
 package com.training;
 
+import static com.training.BoatTypes.*;
+
 /**
  * Created by adame on 1/27/2017.
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I'm in yer main");
+        BoatFactory boatFactory = new BoatFactory();
+
+        Boat myBoat = boatFactory.makeBoat(TUGBOAT);
+
+        Boat myBoat2 = boatFactory.makeBoat(SPEEDBOAT);
+
+        System.out.println(myBoat.toString());
+        System.out.println(myBoat2.toString());
     }
 }
